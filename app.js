f351b53e8146003bf067b0c7591a987c1a6ed940
Mónica Ojeda <monica2519@gmail.com>
2017-09-26 12:31:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var dinosaurs = [];
+
+//Agregando elementos
+
+dinosaurs[0] = 'Tiranosaurio Rex';
+dinosaurs[1] = 'Velociraptor';
+dinosaurs[2] = 'Triceratops';
+dinosaurs[3] = 'Estegosaurio';
+dinosaurs[4] = 'Troodon';
+dinosaurs[5] = ''
+console.log(dinosaurs);
